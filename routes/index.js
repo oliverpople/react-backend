e9f323e20ccc1255853a73d50c6c57114bf9b9a1
oliverpople <oliverpople@gmail.com>
2018-02-13 09:58:29 +0000

    Adding boiler plate example.

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/*middleware CORS headers, allow node data to load from different port*/
+var app = express();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
